62b1a5dbe5cd98f51a23cfa26a6f74edfbaecb7d
yusuf <yeed060118@gmail.com>
2024-04-28 19:41:18 +0300

    Aritmetik Operatorler

---
+++ 7.Aritmetik Operatorler/app.js
@@ -0,0 +1,53 @@
+
+/* 
+    Aritmetik Operatorler  
+    + 
+    - 
+    * 
+    /
+    % 
+    ++
+    -- 
+    ** 
+*/
+
+console.log(2 + 3); // 5
+
+let not1 = 10;
+let not2 = 20;
+console.log(not1 + not2); // 30
+console.log("notlarinizin toplami: " + (not1 + not2));// notlarinizin toplami: 30
+
+let sayi1 = "5";
+let sayi2 = 7;
+console.log(sayi1 + sayi2); // 57
+
+let sayi3 = 5;
+let sayi4 = 2;
+console.log(sayi3 - sayi4); // 3
+
+let sayi5 = 5;
+let sayi6 = 2;
+console.log(sayi5 * sayi6); // 10
+
+let sayi7 = 5;
+let sayi8 = 2;
+console.log(sayi7 / sayi8); // 2.5
+
+let sayi9 = 5;
+let sayi10 = 2;
+console.log(sayi9 % sayi10); // 1 (modulus)
+
+let sayi11 = 5;
+let sayi12 = 2;
+console.log(sayi11 ** sayi12); // 25 (üs)
+
+
+// ++ : sayiyi 1 artirir
+let sayi13 = 5;
+sayi13++; // 6
+
+// -- : sayiyi 1 azaltir
+let sayi14 = 5;
+sayi14--; // 4
+
