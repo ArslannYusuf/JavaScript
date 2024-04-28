130b53434762e7a81ef78ded49337070f515a3fe
yusuf <yeed060118@gmail.com>
2024-04-28 19:54:11 +0300

    atama operatorleri

---
+++ 8.Atama Operatorleri/app.js
@@ -0,0 +1,20 @@
+/* 
+------ Atama Operatorleri ------
+
+    =   --> atama operatoru
+    ==  --> esitlik operatoru
+    === --> esit esitlik operatoru 
+    +=  --> toplayip atama operatoru
+    -=  --> cikarip atama operatoru
+    *=  --> carpip atama operatoru
+    /=  --> bolup atama operatoru
+    %=  --> kalanini alip atama operatoru
+    **= --> us alip atama operatoru
+   
+   NOT: == oeparatorunde karsilastirilan degerlerin ayni olup olmadigini kontrol eder, 
+         degerlerin veri turu ayni olmak zorunda degildir.
+   
+        === oparatorunda deger ve veri turu kontrolu yapar.
+   
+*/
+   
