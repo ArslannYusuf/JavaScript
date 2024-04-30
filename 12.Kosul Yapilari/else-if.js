d66a862a3e057d8c5145656936f832ab0c0c4262
yusuf <yeed060118@gmail.com>
2024-04-30 11:40:13 +0300

    else if yapisi

---
+++ 12.Kosul Yapilari/else-if.js
@@ -0,0 +1,13 @@
+// 3 farkli yol ornegi ile if else yapisi
+
+let yol = prompt("Lutfen gitmek istediginiz yolu 1 - 2 - 3 olarak yaziniz: ");
+
+if (yol == 1) {
+  alert("Secilen yol " + yol + ". yoldur.");
+} else if (yol == 2) {
+  alert("Secilen yol " + yol + ". yoldur.");
+} else if (yol == 3) {
+  alert("Secilen yol " + yol + ". yoldur.");
+} else {
+  alert("Hatali secim yaptiniz. Lütfen gecerli yol seciniz.");
+}
