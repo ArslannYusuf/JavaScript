1c9642203a439d71c65a7cf0529e38a88b0766ac
yusuf <yeed060118@gmail.com>
2024-05-01 22:58:17 +0300

    parametresiz methodlar

---
+++ 16.Functions (Methotlar)/1-app.js
@@ -0,0 +1,24 @@
+// Parametresiz ve geriye dondurmeyen method
+
+/* 
+    function functionName() {
+        kod bloklari
+    }
+
+*/
+
+//burada methodu tanimladim
+function yazdir() {
+  console.log("Hello World");
+}
+
+// methodu cagirdim
+yazdir();
+
+
+
+function topla() {
+    console.log(5 + 10);
+}
+
+topla();
