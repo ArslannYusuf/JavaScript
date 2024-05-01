c90627052e237cb9dbe052023cd4a9e7ea780980
yusuf <yeed060118@gmail.com>
2024-05-02 02:45:50 +0300

    Binary To Decimal Converter

--- day16-Functions (Methotlar)/7-app.js
+++ day16-Functions (Methotlar)/7-app.js
@@ -12,7 +12,9 @@ function convertBinaryToDecimal(binary) {
     let toplam = 0;
     let ust = 0;
     for (let i = binary.length - 1; i >= 0; i--) { 
-        toplam += Number(binary.charAt(i)) * Math.pow(2, ust);
+        if (Number(binary.charAt(i))!=0) {
+          toplam += Number(binary.charAt(i)) * Math.pow(2, ust);
+        }
         ust++;
     }
     console.log("Sonuç: " + toplam);
