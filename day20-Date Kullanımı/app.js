7ae80dec71c42f0642cc1e882fb8a14b6a0c1794
yusuf <yeed060118@gmail.com>
2024-05-04 01:54:49 +0300

    date metotları

---
+++ day20-Date Kullanımı/app.js
@@ -0,0 +1,30 @@
+// Date Kullanımı
+
+let tarih = new Date(); // anlık olarak tarihi ve saati alır
+console.log(tarih); // 2021-06-29T16:33:10.000Z
+
+/* 
+    **** GET METHODS ****
+    getFullYear()	    Yıl bilgisini alır. (yyyy)	
+    getMonth()	        Ay bilgisini 0 ile 11 arasında rakam şeklinde getirir.
+    getDate()	        Gün bilgisini 1 ile 31 arasında rakam şeklinde getirir.	
+    getHours()	        Saat bilgisini 0 ile 23 arasında getirir.	
+    getMinutes()	    Dakika bilgisini 0 ile 59 arasında getirir.
+    getSeconds()	    Saniye bilgisini 0 ile 59 arasında getirir.	
+    getMilliseconds()	Salise bilgisini 0 ile 999 arasında getirir.	
+    getTime()	        Zaman bilgisini 1-1-1970 tarihinden itibaren salise cinsinden getirir.
+    getDay()	        Haftanın kaçıncı gününde (0-6) olduğumuzu verir. 0.gün Pazar	
+    Date.now()	        Şimdiki zaman bilgisini verir.	 	
+    
+
+    **** SET METHODS ****
+    setDate()	        Gün bilgisini günceller. (1-31)
+    setFullYear()	    Yıl bilgisini günceller. 	
+    setHours()	        Saat bilgisini günceller.	
+    setMilliseconds()	Salise bilgisini günceller.	
+    setMinutes()	    Dakika bilgisini günceller.	
+    setMonth()	        Ay bilgisini günceller.
+    setSeconds()	    Saniye bilgisini günceller.	
+    setTime()	        Zaman bilgisini 1-1-1970 tarihinden itibaren salise cinsinden günceller.
+
+*/
