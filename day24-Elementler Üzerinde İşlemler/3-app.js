9f447d2f0b9b47b20399c917a877a2f786ae1556
yusuf <yeed060118@gmail.com>
2024-05-05 17:31:19 +0300

    elementleri silme

---
+++ day24-Elementler Üzerinde İşlemler/3-app.js
@@ -0,0 +1,31 @@
+// Element Silme
+
+const todoList = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
+const todo1 = document.querySelector(".list-group-item");
+
+
+// 1. yol - remove() ile silme
+//  todos[0].remove();
+//  todos[1].remove();
+//  todos[todos.length-1].remove();
+
+//  todo1.remove();
+
+//---------------------------------
+
+// 2. yol - removeChild() ile silme
+//  todoList.removeChild(todos[0]); // todo1 silindi -  uzun yontem
+//  todoList.removeChild(todoList.lastElementChild); // sonuncu todo silindi
+
+
+
+
+
+
+
+
+
+console.log(todos);
+
+
