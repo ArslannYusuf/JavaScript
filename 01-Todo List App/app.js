f3aee4c7e581a64b4c97b7e23f32b6d2f3e7f66c
yusuf <yeed060118@gmail.com>
2024-05-07 21:33:02 +0300

    todo list app

---
+++ 01-Todo List App/app.js
@@ -0,0 +1,68 @@
+// tum secimleri secme
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".list-group")[0];
+const secondCardBody = document.querySelectorAll(".list-group")[1];
+const clearButton = document.querySelector("#clearButton");
+todos = [];
+
+runEvents();
+
+function runEvents() {
+    form.addEventListener("submit", addTodo);
+
+}
+
+function addTodo(e) { 
+    const inputText = addInput.value.trim();
+    if (inputText== null || inputText === "") {
+        alert("Lütfen bir değer giriniz!");
+    } else {
+        // arayuze ekleme
+        addTodoToUI(inputText);
+    }
+    
+
+    // storage ekleme
+    addTodoToStorage(inputText);
+
+
+    //yonlendirmeyi engelleme
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo) {
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between";
+    li.textContent = newTodo;
+
+    const a = document.createElement("a");
+    a.href = "#";
+    a.className = "delete-item";
+
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value = "";
+
+}
+
+function addTodoToStorage(newTodo) { 
+    checkTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function checkTodosFromStorage() {
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+}
