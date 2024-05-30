e99076e907c9ee71d19f749022e46f72a3384418
Yusuf Arslan <yeed060118@gmail.com>
2024-05-30 16:40:46 +0300

    promise && XmlHttpRequest

--- day34-Sync & Async/04-app.js
+++ day34-Sync & Async/04-app.js
@@ -1,5 +1,7 @@
 // PROMİSE + XMLHTTPREQUEST
 
+
+// user' lari getir
 function getUsers(url){
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
@@ -22,10 +24,53 @@ function getUsers(url){
 
 getUsers("https://jsonplaceholder.typicode.com/users").then((res)=>{
     //console.log(res)
-    res.array.forEach((user) => {
+    res.forEach((user) => {
+        //user'larin isimlerini yazdir
         console.log(user.name)
     });
-    console.log("daha sonra asenkron kodlarimi burada calistirabilirim")
+
+    console.log("daha sonra farkli asenkron kodlarimi burada calistirabilirim")
 }).catch((err)=>console.log(err)).finally(()=>{
     // her durumda calisir olacagi icin, ornegin mail atma kodlarini buraya yazabilirim
 })
+
+
+// ---------------
+
+
+
+// id'si 3 olan user'in postlarindaki yorumlari getir
+
+function getCommentsByUserId(url){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", () => {
+           try {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            }
+           } catch (error) {
+            reject(error);
+           }
+        })
+
+        
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+
+getUsers("https://jsonplaceholder.typicode.com/users/3")
+.then((res)=>{
+    console.log(res)
+   return getCommentsByUserId(`https://jsonplaceholder.typicode.com/users/${res.id}`)
+})
+.then((res)=>{
+    console.log(res)
+})
+.catch((err)=>console.log(err)).finally(()=>{
+    // her durumda calisir olacagi icin, ornegin mail atma kodlarini buraya yazabilirim
+})
+
